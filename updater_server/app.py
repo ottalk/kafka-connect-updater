e54fa90be34b469be6b5433c1b35e56cc6816021
Kuldip Ottal <kuldip.ottal@gmail.com>
2022-11-12 16:59:41 +0000

    working version of client and server updater

--- updater_server/app.py
+++ updater_server/app.py
@@ -2,6 +2,10 @@ from flask import Flask,request,jsonify
 import requests
 import json
 
+headers = {
+    'Content-Type': 'application/json'
+}
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -37,11 +41,24 @@ def create_update_connectors():
         config_files = request.files.getlist("config_files")
         print("#config_files="+str(len(config_files)))
         for config_file in config_files:
-            print(config_file.filename)
-            with open(config_file) as config_json_file:
-                config_json = json.load(config_json_file)
+            config_json = json.load(config_file)
+            print("config file name="+config_file.filename)
+
+            config=config_file.filename.split(".")[0]
+            print("config="+config)
+            if config in connectors_list.text:
+                print("PUT")
+                put_config_json=config_json['config']
+                print(put_config_json)
+                put_response=requests.put('http://localhost:8083/connectors/'+config+'/config',headers=headers, json=put_config_json)
+                print("put_response="+put_response.text)
+            else:
+                print("POST")
                 print(config_json)
-    return "COMPLETED"
+                post_response=requests.post('http://localhost:8083/connectors',headers=headers, json=config_json)
+                print("post_response="+post_response.text)
+
+    return "COMPLETED\n"
 
 if __name__ == '__main__':
     app.run(debug=True,port=8088)
